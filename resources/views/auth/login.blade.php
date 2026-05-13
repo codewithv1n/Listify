0e809fb9eb10b8ba94d0bc0b10d486b59a952bcc
codewithv1n <vinzelmarano@gmail.com>
2026-05-13 20:14:07 +0800

    UPDATE: integrate to backend that can insert and validate user req

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,20 +12,25 @@
   <div class="bg-white rounded-lg p-12 w-95 ">
     <label for="login" class="mb-7 block text-center font-bold text-3xl">Login</label>
 
-    <label for="username" class="mb-2 block  font-medium">Username</label>
-    <input type="text" class="rounded-sm border-black p-2 px-8"
+   <form action="{{route('login.post')}}" method="POST">
+    @csrf
+     <label for="username" class="mb-4 block font-medium">Username</label>
+    <input type="text" name="username" class="rounded-sm border-black p-2 px-8 mb-4"
     placeholder="Enter your username">
 
-    <label for="password" class="mt-2 block font-medium">Password</label>
-    <input type="password" class="rounded-sm border-black p-2 px-8 mt-2 "
+    <label for="password" class="mb-3 block font-medium">Password</label>
+    <input type="password" name="password" class="rounded-sm border-black p-2 px-8 mt-2 mb-2 "
     placeholder="Enter your password">
 
 
     <button class="uppercase rounded-sm p-2 text-white bg-gray-500 px-8 block mx-auto mt-5"  
     type="submit">Login</button>
 
+    
     <p class="text-sm mt-3 ">Don't you have an account click <a href="/signup" class="underline text-blue-500 text-sm m-1 ">Signup</a></p>
 
+   </form>
+   
   </div>
  </div>
 
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -8,23 +8,29 @@
     </head>
     <body>
         
-       <div class="min-h-screen bg-gray-200 flex items-center justify-center">
-       <div class="bg-white rounded-lg p-12 w-95">
-        <label for="signup" class="mb-7 text-3xl block text-center font-bold ">Signup</label>
+<div class="min-h-screen bg-gray-200 flex items-center justify-center">
+ <div class="bg-white rounded-lg p-12 w-95">
+    <label for="signup" class="mb-7 text-3xl block text-center font-bold ">Signup</label>
 
-        <label for="username" class="block mb-2 text-sm font-medium mt-4">Username</label>
-        <input type="text" class="rounded-sm border-black p-2 px-8 mt-2 " 
+    
+   <form action="{{route('signup.post')}}" method="POST">
+    @csrf
+        <label for="name" class="block mb-2 font-medium mt-4">Name</label>
+       <input type="text" name="name" class="rounded-sm border-black p-2 px-8 mt2"
+       placeholder="Enter your name">
+
+        <label for="username" class="block mb-2  font-medium mt-4">Username</label>
+        <input type="text" name="username" class="rounded-sm border-black p-2 px-8 mt-2 " 
         placeholder="Enter your username">
+       
 
-
-        <label for="password" class="block mb-2 text-sm font-medium mt-4">Password</label>
-        <input type="password" class="rounded-sm border-black p-2 px-8 mt-2 " 
+        <label for="password" class="block mb-2 font-medium mt-4">Password</label>
+        <input type="password" name="password" class="rounded-sm border-black p-2 px-8 mt-2 " 
         placeholder="Enter your password">
+      
 
-
-
-        <label for="confirm_password" class="block mb-2 text-sm font-medium mt-3">Confirm Password</label>
-        <input type="password" class="rounded-sm border-black p-2 px-8 mt-2" placeholder="Please confirm your password">
+        <label for="confirm_password" class="block mb-2 font-medium mt-3">Confirm Password</label>
+        <input type="password" name="password_confirmation" class="rounded-sm border-black p-2 px-8 mt-2" placeholder="Please confirm your password">
 
 
         <button class="mt-4 uppercase block mx-auto rounded-sm  text-white bg-gray-500 p-2 px-8" 
@@ -32,11 +38,9 @@
 
 
         <p class="text-sm mt-3">You have an account click <a href="/" class="underline text-sm text-blue-500 ">Login</a> to enjoy experience</p>
- 
-      </div>
+   </form>
 
-        
-     </div>
-        
-    </body>
+  </div>
+ </div>
+</body>
 </html>
